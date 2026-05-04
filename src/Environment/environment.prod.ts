438750e75af0c5cf424fd027e78f259636167c0a
Anas-Ali-Wali <perwali0123@gmail.com>
2026-05-04 01:32:32 -0700

    new work

--- src/Environment/environment.prod.ts
+++ src/Environment/environment.prod.ts
@@ -1,8 +1,12 @@
 
 
 export const environment = {
-  //   apiUrl: 'https://fms-sheikh-api.persistentsolutions.co/api/',
   production: true,
-  apiUrl: 'http://localhost:5025/api'
+  // apiUrl: 'http://localhost:5025/api'
+
+    apiUrl: 'http://anasali1-001-site1.ltempurl.com/api'  // ✅ Live URL
+
 
 };
+
+  //   apiUrl: 'https://fms-sheikh-api.persistentsolutions.co/api/',
