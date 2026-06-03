714193cd70aeb31f104b1ede6765be36d01d54d0
Anas-Ali-Wali <perwali0123@gmail.com>
2026-06-03 03:45:01 -0700

    Capacitor Android setup completed

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smarteraso.admin',
+  appName: 'E-Commerce',
+  webDir: 'dist/admin-apk'
+};
+
+export default config;
